2149fccf3cbefb349334044b637e19f8d4bf54fe
Mark Leistra <markdleist@gmail.com>
2026-01-31 12:31:53 -0500

    Adding Digraph class and testing system

---
+++ helpers/__init__.py
@@ -0,0 +1,4 @@
+from .graphs import Digraph
+from .handle_input import q2_input
+
+__all__ = [Digraph, q2_input]
---
+++ tests.py
@@ -0,0 +1,28 @@
+import sys
+import fileinput
+from Q2.part_a import main
+from helpers import q2_input
+
+if __name__ == "__main__":
+    for arg in sys.argv[1:]:
+        if arg == "1":
+            print("Testing Q1")
+        elif arg == "2":
+            print("Testing Q2A")
+            input = fileinput.input("tests/q2a_test1.in")
+            g, s = q2_input(input)
+            # Solve Q2A
+            input.close()
+
+            input = fileinput.input("tests/q2a_test2.in")
+            g, s = q2_input(input)
+            # Solve Q2A
+            input.close()
+        elif arg == "3":
+            print("Testing Q2B")
+            input = fileinput.input("tests/q2a_test1.in")
+            g, s = q2_input(input)
+            # Solve Q2B
+            input.close()
+        else:
+            print("Please provide at least 1 argument of 1, 2, or 3.")
